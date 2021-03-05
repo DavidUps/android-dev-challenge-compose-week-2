ba3aba94c72e6b79179674dfb52d90047288243e
DavidUps <darribas_talentomobile@bankinter.com>
2021-03-05 23:20:39 +0100

    readme and fix spotless

--- app/src/main/java/com/example/androiddevchallenge/ui/screens/countdouwn/CountDownScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/countdouwn/CountDownScreen.kt
@@ -1,8 +1,21 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.screens.countdouwn
 
-import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,14 +23,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Snackbar
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -68,20 +77,21 @@ fun CountDownScreen(viewModel: CountDownViewModel = viewModel()) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
-            Button(onClick = {
-                when {
-                    minutes.isNotEmpty() && seconds.isNotEmpty() -> startCountDown(
-                        viewModel,
-                        minutes,
-                        seconds
-                    )
-                    minutes.isNotEmpty() -> startCountDown(viewModel, minutes, "0")
-                    seconds.isNotEmpty() -> startCountDown(viewModel, "0", seconds)
-                    else -> Toast.makeText(context, "fill all edit texts", Toast.LENGTH_SHORT)
-                        .show()
-
+            Button(
+                onClick = {
+                    when {
+                        minutes.isNotEmpty() && seconds.isNotEmpty() -> startCountDown(
+                            viewModel,
+                            minutes,
+                            seconds
+                        )
+                        minutes.isNotEmpty() -> startCountDown(viewModel, minutes, "0")
+                        seconds.isNotEmpty() -> startCountDown(viewModel, "0", seconds)
+                        else -> Toast.makeText(context, "fill all edit texts", Toast.LENGTH_SHORT)
+                            .show()
+                    }
                 }
-            }) {
+            ) {
                 Text(text = "Start")
             }
         }
